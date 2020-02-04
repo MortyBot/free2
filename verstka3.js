4901e63a7d23446008f1e96cd815f4cb9b5bc0c1
MortyBot <aleksandr.kisko@mail.ru>
2020-02-04 20:27:19 +0300

    create project

---
+++ verstka3.js
@@ -0,0 +1,16 @@
+
+/* var year = prompt("сколько тебе лет");
+if (year == 18) { 
+    alert("Правильно");
+
+}
+else {
+    alert("Не правильно");
+} */
+
+
+    document.getElementById("knopka2__3").onclick = function (e) {
+        document.getElementById("pole1").value = "";
+        document.getElementById("pole2").value = "";
+        document.getElementById("pole3").value = "";
+    }
